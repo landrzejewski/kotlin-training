15058ebcdb3728a91b6018febba9b97f22802196
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-26 11:03:59 +0200

    m1

--- src/main/kotlin/pl/training/exercies.kt
+++ src/main/kotlin/pl/training/exercies.kt
@@ -3,6 +3,6 @@
 // Napisz funkcję weryfikującą czy dany rok jest przestępny
 // https://docs.microsoft.com/en-us/office/troubleshoot/excel/determine-a-leap-year
 
-// Napisz funkcję sprawdzająca, czy dany znak to litera (duża lub mala ASCII) lub cyfra z wykorzystaniem range
-
 // Napisz program działający jak prosty kalkulator konsolowy
+
+// Napisz funkcję sprawdzająca, czy dany znak to litera (duża lub mala ASCII) lub cyfra z wykorzystaniem range
