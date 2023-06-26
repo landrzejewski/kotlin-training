5245d66d197e54ce840b447e265ed5a239b5ac62
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-26 11:01:52 +0200

    m1

---
+++ src/main/kotlin/pl/training/exercies.kt
@@ -0,0 +1,8 @@
+// Napisz funkcję zliczającą ilość wystąpień danej litery w słowie/zdaniu
+
+// Napisz funkcję weryfikującą czy dany rok jest przestępny
+// https://docs.microsoft.com/en-us/office/troubleshoot/excel/determine-a-leap-year
+
+// Napisz funkcję sprawdzająca, czy dany znak to litera (duża lub mala ASCII) lub cyfra z wykorzystaniem range
+
+// Napisz program działający jak prosty kalkulator konsolowy
