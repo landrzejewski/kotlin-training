3601fa7e6d070d055b1c9d76728c061318363975
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-26 15:02:10 +0200

    m1

--- src/main/kotlin/pl/training/main.kt
+++ src/main/kotlin/pl/training/main.kt
@@ -1,7 +1,7 @@
 package pl.training.fn
 
 // Pure function
-// - dla tego samego wejścia dostaje to samo wyjście
+// - dla tego samego wejścia zwraca to samo wyjście
 // - nie ma efektów ubocznych
 // - transparentość referencyjna abs(-3) == 3
 fun abs(value: Int) = if (value < 0) -value else value
@@ -10,7 +10,6 @@ fun multiAdd(a: Int, b: Int) = a + b
 fun add(a: Int): (Int) -> Int = { b -> a + b }
 
 // Rekurencja zamiast pętli
-
 fun factorial(number: Int): Int {
     tailrec fun loop(n: Int, result: Int = 1): Int = if (n <= 0) result else loop(n - 1, n * result)
     return loop(number)
@@ -33,6 +32,9 @@ fun <E> findFirst(xs: Array<E>, predicate: Predicate<E>): Int {
 
 fun isEven(value: Int) = value % 2 == 0
 
+// Partial application
+fun <A, B, C> partial(a: A, fn: (A, B) -> C): (B) -> C = { b -> fn(a, b) }
+
 // Curring and composition
 fun <A, B, C> curry(fn: (A, B) -> C): (A) -> (B) -> C = { a: A -> { b: B -> fn(a, b) } }
 fun <A, B, C> uncurry(fn: (A) -> (B) -> C): (A, B) -> C = { a: A, b: B -> fn(a)(b) }
@@ -42,7 +44,7 @@ fun main() {
     println(add2(3))
     println(add2(4))
 
-    println("Facctorial: ${factorial(5)}")
+    println("Factorial: ${factorial(5)}")
 
     println(formatResult(-5, ::abs))
     println(formatResult(5, add(2)))
@@ -51,10 +53,13 @@ fun main() {
     findFirst(numbers) { it > 2 }
     findFirst(numbers, ::isEven)
 
+    val add4 = partial(4, ::multiAdd)
+    println(add4(4))
+
     val curriedMultiAdd = curry(::multiAdd)
-    val add3 = curriedMultiAdd(3)
-    println(add3(3))
-    println(add3(4))
+    val add5 = curriedMultiAdd(3)
+    println(add5(3))
+    println(add5(4))
 }
 
 
