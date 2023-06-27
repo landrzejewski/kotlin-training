97cc075df008cd0405a6630155d05832e12583fe
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-27 14:03:03 +0200

    m1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,21 @@ repositories {
     mavenCentral()
 }
 
+val retrofitVersion = "2.9.0"
+
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.22")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
 
-    testImplementation("io.kotest:kotest-runner-junit5:4.6.1")
-    testImplementation("io.kotest:kotest-assertions-core:4.6.1")
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
 }
 
 tasks.test {
---
+++ src/main/kotlin/pl/training/TicTacToe.kt
@@ -0,0 +1,55 @@
+package pl.training
+
+import pl.training.Player.CROSS
+import java.util.Collections.disjoint
+
+class TicTacToe(private var crossFields: MutableSet<Int> = mutableSetOf(), private var circleFields: MutableSet<Int> = mutableSetOf(), player: Player = CROSS) {
+
+    var player = CROSS
+        private set
+    val isGameOver: Boolean
+        get() = allFieldsAreTake() || playerTookWinningSequence()
+    val winner: Player?
+        get() = getFinalWinner()
+
+    init {
+        if (!disjoint(crossFields, circleFields)) {
+            throw IllegalArgumentException()
+        }
+        this.player = player
+    }
+
+    private fun allFieldsAreTake() = BOARD_SIZE - crossFields.size - circleFields.size <= 0
+
+    private fun playerTookWinningSequence() = winningSequences.any { playerFields().containsAll(it) }
+
+    fun makeMove(field: Int): Boolean {
+        if (!isOnBoard(field) || isTaken(field)) return false
+        playerFields().add(field)
+        if (!isGameOver) {
+            player = player.reverse()
+        }
+        return true
+    }
+
+    private fun playerFields() = if (player == CROSS) crossFields else circleFields
+
+    private fun isOnBoard(field: Int) = field in 1..9
+
+    private fun isTaken(field: Int) = field in crossFields.union(circleFields)
+
+    private fun getFinalWinner() = if (isGameOver) player else null
+
+    companion object {
+
+        private const val BOARD_SIZE = 9
+
+        private val winningSequences = setOf(
+            setOf(1, 2, 3), setOf(4, 5, 6), setOf(7, 8, 9),
+            setOf(1, 4, 7), setOf(2, 5, 8), setOf(3, 6, 9),
+            setOf(1, 5, 9), setOf(3, 5, 7)
+        )
+
+    }
+
+}
