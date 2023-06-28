1ef3a26c85c5c43bae0a93b8a898e7f371196c88
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-28 13:29:25 +0200

    m1

--- src/main/kotlin/pl/training/payments/domain/Money.kt
+++ src/main/kotlin/pl/training/payments/domain/Money.kt
@@ -3,14 +3,9 @@ package pl.training.payments.domain
 import java.math.BigDecimal
 import java.util.*
 
-class Money(amount: BigDecimal, currency: Currency) {
-
-    val amount: BigDecimal
-    val currency: Currency
+class Money(val amount: BigDecimal, val currency: Currency) {
 
     init {
-        this.currency = currency
-        this.amount = amount
         require(amount.compareTo(BigDecimal.ZERO) >= 1)
     }
 
@@ -24,30 +19,22 @@ class Money(amount: BigDecimal, currency: Currency) {
         return Money(amount.subtract(money.amount), currency)
     }
 
-    fun multiplyBy(value: Int): Money {
-        return Money(amount.multiply(BigDecimal.valueOf(value.toLong())), currency)
-    }
+    fun multiplyBy(value: Int) = Money(amount.multiply(BigDecimal.valueOf(value.toLong())), currency)
 
     fun isGreaterOrEqual(money: Money): Boolean {
         checkCurrencyCompatibility(money)
         return amount.compareTo(money.amount) > -1
     }
 
-    private fun checkCurrencyCompatibility(money: Money) {
-        require(currency == money.currency)
-    }
+    private fun checkCurrencyCompatibility(money: Money) = require(currency == money.currency)
 
     companion object {
 
         private val DEFAULT_CURRENCY: Currency = Currency.getInstance("PLN")
 
-        fun of(value: BigDecimal): Money {
-            return Money(value, DEFAULT_CURRENCY)
-        }
+        fun of(value: BigDecimal) = Money(value, DEFAULT_CURRENCY)
 
-        fun of(value: BigDecimal, currency: Currency): Money {
-            return Money(value, currency)
-        }
+        fun of(value: BigDecimal, currency: Currency) = Money(value, currency)
 
     }
 
