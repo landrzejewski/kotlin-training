c3ed39bbcd13eda8fc78cba1c22aef629a38b1f0
landrzejewski <landrzejewski.poczta@gmail.com>
2023-06-28 12:35:46 +0200

    m1

--- src/main/kotlin/pl/training/parsers.kt
+++ src/main/kotlin/pl/training/parsers.kt
@@ -18,6 +18,22 @@ fun keyValuePair(text: String) = text
 sealed class Result<out T> {
     data class Success<T>(val match: T, var remainder: String): Result<T>()
     data class Failure(val expected: String, var remainder: String): Result<Nothing>()
+
+    fun <U> map(f: (T) -> U): Result<U> = when (this) {
+        is Failure -> this
+        is Success -> Success(f(match), remainder)
+    }
+
+    fun <U> flatMap(f: (T, String) -> Result<U>): Result<U> = when (this) {
+        is Failure -> this
+        is Success -> f(match, remainder)
+    }
+
+    fun mapExpected(f: (String) -> String): Result<T> = when (this) {
+        is Success -> this
+        is Failure -> Failure(f(expected), remainder)
+    }
+
 }
 
 typealias Parser<T> = (String) -> Result<T>
@@ -50,17 +66,75 @@ fun whitespace(input: String): Result<String> {
     }
 }
 
+/*fun <T1, T2> sequence(firstParser: Parser<T1>, secondParser: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    when(val firstResult = firstParser(input)) {
+        is Failure -> firstResult
+        is Success -> when (val secondResult = secondParser(firstResult.remainder)) {
+            is Failure -> secondResult
+            is Success -> Success(firstResult.match to secondResult.match, secondResult.remainder)
+        }
+    }
+}*/
 
+/*fun <T1, T2> sequence(firstParser: Parser<T1>, secondParser: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    when(val firstResult = firstParser(input)) {
+        is Failure -> firstResult
+        is Success -> secondParser(firstResult.remainder).map { firstResult.match to it }
+    }
+}*/
 
+fun <T1, T2> sequence(firstParser: Parser<T1>, secondParser: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    firstParser(input).flatMap { firstResult, remainder ->
+        secondParser(remainder).map { firstResult to it }
+    }
+}
 
+infix fun <T1, T2> Parser<T1>.then(otherParser: Parser<T2>): Parser<Pair<T1, T2>> = sequence(this, otherParser)
 
+fun <T> oneOf(firstParser: Parser<T>, secondParser: Parser<T>): Parser<T> = { input ->
+    when (val firstResult = firstParser(input)) {
+        is Success -> firstResult
+        is Failure -> secondParser(input).mapExpected { "${firstResult.expected}, $it" }
+    }
+}
 
+infix fun <T> Parser<T>.or(otherParser: Parser<T>): Parser<T> = oneOf(this, otherParser)
 
+fun <T, U> Parser<T>.map(f: (T) -> U): Parser<U> = { this(it).map(f) }
 
+infix fun <X, T> Parser<X>.skipLeft(p: Parser<T>): Parser<T> = sequence(this, p).map { it.second }
 
+infix fun <T, Y> Parser<T>.skipRight(y: Parser<Y>): Parser<T> = sequence(this, y).map { it.first  }
 
+fun <T> Parser<T>.many(): Parser<List<T>> = { input ->
+    when (val result = this(input)) {
+        is Failure -> Success(emptyList(), input)
+        is Success -> many()(result.remainder).map { listOf(result.match) + it }
+    }
+}
 
+infix fun <T, X> Parser<T>.separatedBy(separatorParser: Parser<X>) : Parser<List<T>> = { input ->
+    fun parse(tail: String): Result<List<T>> = when (val separatorResult = separatorParser(tail)){
+        is Failure -> Success(emptyList(), tail)
+        is Success -> when (val result = this(separatorResult.remainder)) {
+            is Failure -> result
+            is Success -> parse(result.remainder).map { listOf(result.match) + it }
+        }
+    }
+    when (val result = this(input)) {
+        is Failure -> Success(emptyList(), input)
+        is Success -> parse(result.remainder).map { listOf(result.match) + it }
+    }
+}
 
+fun prefixWhile(predicate: (Char) -> Boolean): Parser<String> = { input ->
+    val match = input.takeWhile(predicate)
+    if (match.isNotEmpty()) {
+        Success(match, input.dropPrefix(match))
+    } else {
+        Failure("Expected prefix", input)
+    }
+}
 
 
 
--- src/test/kotlin/pl/training/ParsersTest.kt
+++ src/test/kotlin/pl/training/ParsersTest.kt
@@ -34,5 +34,63 @@ class ParsersTest : StringSpec({
         whitespace("jan") shouldBe Failure("Expected: one or more whitespaces", "jan")
     }
 
+    "should parse sequence" {
+        //val sequenceParser = sequence(prefix("-"), ::int)
+        val sequenceParser = prefix("-") then ::int
+        sequenceParser("-123jan") shouldBe Success(Pair("-", 123), "jan")
+        sequenceParser("123jan") shouldBe Failure("Expected: \"-\" prefix", "123jan")
+    }
+
+    "should parse one of" {
+        //val onOfParser = oneOf(prefix("a"), prefix("b"))
+        val onOfParser = prefix("a") or (prefix("b"))
+        onOfParser("ab") shouldBe Success("a", "b")
+        onOfParser("bc") shouldBe Success("b", "c")
+        onOfParser("cd") shouldBe Failure("Expected: \"a\" prefix, Expected: \"b\" prefix", "cd")
+    }
+
+    "should map result" {
+        int("11").map { it % 2 == 0 } shouldBe Success(false, "")
+    }
+
+    "should skip left parser" {
+        val parser = ::int skipLeft (prefix("a"))
+        parser("1a") shouldBe Success("a", "")
+    }
+
+    "should skip right parser" {
+        val parser = prefix("a") skipRight (::int)
+        parser("a1") shouldBe Success("a", "")
+    }
+
+    "should parse many" {
+        val manyPrefixes = prefix("a").many()
+        manyPrefixes("aaa") shouldBe Success(listOf("a", "a", "a"), "")
+    }
+
+    "should parse many with separator" {
+        val manyWithSeparatorParser = ::int.separatedBy(prefix(","))
+        manyWithSeparatorParser("1,2") shouldBe Success(listOf(1, 2), "")
+        manyWithSeparatorParser("1,a") shouldBe Failure("Expected: integer value", "a")
+        manyWithSeparatorParser("a") shouldBe Success(emptyList(), "a")
+    }
+
+    "should parse expression with array of ints" {
+        val text = "let  ab = [1, 2, 3,  4]"
+
+        val manySpaces = ::whitespace.many()
+        val let = prefix("let") then ::whitespace.many()
+        val variableName = prefixWhile { it.isLetter() } skipRight manySpaces
+        val assigment = prefix("=") skipRight manySpaces
+        val numbers = ::int separatedBy sequence(prefix(","), manySpaces)
+        val array = prefix("[") skipLeft numbers skipRight prefix("]")
+        val parser = let skipLeft variableName skipRight assigment then array
+
+        when (val result = parser(text)) {
+            is Success -> println(result.match)
+            is Failure -> println(result.expected)
+        }
+    }
+
 })
 
